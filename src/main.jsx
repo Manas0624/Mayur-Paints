5fbcd5ea4c1f074fb49ebd9b901686996b9ed338
manasshinde369 <manasshinde369@gmail.com>
2026-04-21 01:33:16 +0530

    Remove Redux persist - focus on order placement fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,37 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { store, persistor } from './redux/store'
+import store from './redux/store'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 import './index.css'
 
-const LoadingScreen = () => (
-  <div style={{ 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    height: '100vh',
-    fontSize: '18px',
-    color: '#E85D26'
-  }}>
-    Loading...
-  </div>
-)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
-        <BrowserRouter>
-          <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { combineReducers } from 'redux'
 import authReducer from './authSlice'
 import cartReducer from './cartSlice'
 
-// Persist configuration
-const persistConfig = {
-  key: 'mayurpaints',
-  storage,
-  whitelist: ['cart', 'auth'], // Only persist cart and auth
-}
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-  cart: cartReducer,
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+  },
 })
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
-      },
-    }),
-})
-
-export const persistor = persistStore(store)
 
 export default store
